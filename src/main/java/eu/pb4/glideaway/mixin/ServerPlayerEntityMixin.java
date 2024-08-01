c1eaecc8ba7eee545ae0a2cc9e61363f15de16a1
Mallory Hayr <hey@igalaxy.dev>
2024-07-31 22:36:53 -0700

    Add Distance by Glider statistic

---
+++ src/main/java/eu/pb4/glideaway/mixin/ServerPlayerEntityMixin.java
@@ -0,0 +1,27 @@
+package eu.pb4.glideaway.mixin;
+
+import eu.pb4.glideaway.ModInit;
+import eu.pb4.glideaway.entity.GliderEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stat;
+import net.minecraft.stat.Stats;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayerEntity.class)
+public abstract class ServerPlayerEntityMixin {
+    @Shadow public abstract void increaseStat(Stat<?> stat, int amount);
+
+    @Inject(method = "increaseRidingMotionStats", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/server/network/ServerPlayerEntity;getVehicle()Lnet/minecraft/entity/Entity;", shift = At.Shift.AFTER))
+    private void gliderRidingMotionStat(double deltaX, double deltaY, double deltaZ, CallbackInfo ci) {
+        Entity entity = ((EntityAccessor) this).invokeGetVehicle();
+        if (entity instanceof GliderEntity) {
+            System.out.println("Hey you're riding a glider");
+            this.increaseStat(Stats.CUSTOM.getOrCreateStat(ModInit.GLIDE_ONE_CM), 1);
+        }
+    }
+}
